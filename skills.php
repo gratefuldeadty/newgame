93d473864e3b0cd1c10a2d0792ed454bc7dd24de
gratefuldeadty <gratefuldeadty@gmail.com>
2015-02-15 15:14:52 -0500

    Update skills.php

--- skills.php
+++ skills.php
@@ -1,1 +1,38 @@
 <?php
+
+class Game 
+{
+    private static $engine;
+    
+    // Do not allow object instantiation.
+    private function __construct() {}
+    private function __destruct() {}
+    private function __clone() {}
+    
+    /**
+     * Handle calls to static methods
+     * @param string $name
+     * @param array $params
+     * @return mixed - Callback results.
+     */
+    public static function __callStatic($name, $params)
+    {
+        static $initialized = false;
+        if (!$initialized)
+        {
+            require_once __DIR__.'/autoload.php';
+            self::$engine = new \game\Engine();
+            $initialized = true;
+        }
+        return \game\core\Dispatcher::invokeMethod([self::$engine, $name], $params);
+    }
+    
+    
+    /**
+     * @return object app instance
+     */
+    public static function app()
+    {
+        return self::$engine;    
+    }
+}
